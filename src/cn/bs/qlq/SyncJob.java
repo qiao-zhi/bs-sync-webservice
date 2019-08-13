1c6bcd75193f8e9e871fc20e3fd99ffd6ac88626
qiao_zhi <qiao_liqiang@163.com>
2019-08-13 13:17:07 +0800

    xxx

--- src/cn/bs/qlq/SyncJob.java
+++ src/cn/bs/qlq/SyncJob.java
@@ -1,54 +1,85 @@
 package cn.bs.qlq;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
-import javax.xml.namespace.QName;
-
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
-import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 
+import cn.bs.qlq.utils.BeanUtils;
 import cn.bs.qlq.utils.ResourcesUtil;
+import cn.infohow.mes.ei.remote.ChxOutsideDataRemoteService;
+import cn.infohow.mes.ei.remote.ChxOutsideDataRemoteServiceService;
+import cn.infohow.mes.ei.remote.ManuOrderDetailInfo;
+import cn.infohow.mes.ei.remote.VinGenerateInfo;
 
 public class SyncJob {
 
 	public static void main(String[] args) {
 		syncVinQueue();
+
+		syncOrderList();
 	}
 
 	/**
 	 * 同步 vin队列
 	 */
 	private static void syncVinQueue() {
-		JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
-
-		String vinQueueWSDLUrl = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "vinQueueWSDLUrl"),
-				"http://10.98.100.160:7001/ghis/remote/chxOutsideData/ChxOutsideDataRemoteService?wsdl");
-		String vinQueueNameSpaceUrl = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "vinQueueNameSpaceUrl"),
-				"http://WebXml.com.cn/");
-		String vinQueueMethodName = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "vinQueueNameSpaceUrl"),
-				"selectVinListForService");
-
-		// url为调用webService的wsdl地址
-		Client client = dcf.createClient(vinQueueWSDLUrl);
-		// namespace是命名空间，methodName是方法名
-		QName name = new QName(vinQueueNameSpaceUrl, vinQueueMethodName);
-
 		String currentData = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
 		String startDate = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "vinQueueStartDate"), currentData);
-		String endDate = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "vinQueueStartDate"), "");
+		String endDate = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "vinQueueEndDate"), "");
 
-		Object[] objects;
-		try {
-			objects = client.invoke(name, startDate, endDate);// 第一个参数是上面的QName，第二个开始为参数，可变数组
-			System.out.println(Arrays.toString(objects));
-			FileUtils.writeStringToFile(new File("result.txt"), Arrays.toString(objects), "UTF-8", true);
-		} catch (Exception e) {
-			e.printStackTrace();
+		ChxOutsideDataRemoteServiceService chxOutsideDataRemoteServiceService = new ChxOutsideDataRemoteServiceService();
+		ChxOutsideDataRemoteService chxOutsideDataRemoteService = chxOutsideDataRemoteServiceService
+				.getChxOutsideDataRemoteServicePort();
+		List<VinGenerateInfo> selectVinListForService = chxOutsideDataRemoteService.selectVinListForService(startDate,
+				endDate);
+
+		if (CollectionUtils.isNotEmpty(selectVinListForService)) {
+			try {
+				List<Map<String, Object>> beansToMaps = BeanUtils.beansToMaps(selectVinListForService, true, true);
+				FileUtils.writeStringToFile(new File("result.txt"), beansToMaps.toString(), "UTF-8", true);
+			} catch (Exception ignored) {
+				// ignored
+			}
+
+			System.out.println("vins 数据已保存");
+			return;
 		}
+
+		System.out.println("vins 没有数据");
+	}
+
+	private static void syncOrderList() {
+		String currentData = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
+		String startDate = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "orderListStartDate"),
+				currentData);
+		String endDate = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "orderListSEndDate"), "");
+		String intDay = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "orderListIntDay"), "1");
+		String flowLine = StringUtils.defaultIfBlank(ResourcesUtil.getValue("sync", "orderListFlowLine"), "xtmq0101");
+
+		ChxOutsideDataRemoteServiceService chxOutsideDataRemoteServiceService = new ChxOutsideDataRemoteServiceService();
+		ChxOutsideDataRemoteService chxOutsideDataRemoteService = chxOutsideDataRemoteServiceService
+				.getChxOutsideDataRemoteServicePort();
+		List<ManuOrderDetailInfo> orders = chxOutsideDataRemoteService.selectManuOrderDetilListService(startDate,
+				endDate, intDay, flowLine);
+
+		if (CollectionUtils.isNotEmpty(orders)) {
+			try {
+				List<Map<String, Object>> beansToMaps = BeanUtils.beansToMaps(orders, true, true);
+				FileUtils.writeStringToFile(new File("result2.txt"), beansToMaps.toString(), "UTF-8", true);
+			} catch (Exception ignored) {
+				// ignored
+			}
+
+			System.out.println("orders 数据已保存");
+			return;
+		}
+
+		System.out.println("orders 没有数据");
 	}
 }
